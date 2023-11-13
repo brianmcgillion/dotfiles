8d67c38e3c7b5c1b51c90ffe6a17fc557e84642c
Brian McGillion <bmg.avoin@gmail.com>
2023-11-13 18:31:33 +0400

    Rewrite to flake-parts compatible
    Signed-off-by: Brian McGillion <bmg.avoin@gmail.com>

---
+++ users/bmg.nix
@@ -0,0 +1,14 @@
+# SPDX-License-Identifier: Apache-2.0
+{config, ...}: {
+  users.users = {
+    brian = {
+      isNormalUser = true;
+      description = "Brian";
+      openssh.authorizedKeys.keys = [
+        "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIEJ9ewKwo5FLj6zE30KnTn8+nw7aKdei9SeTwaAeRdJDAAAABHNzaDo="
+        "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIA/pwHnzGNM+ZU4lANGROTRe2ZHbes7cnZn72Oeun/MCAAAABHNzaDo="
+      ];
+      extraGroups = ["networkmanager" "wheel" "dialout" "plugdev"];
+    };
+  };
+}
