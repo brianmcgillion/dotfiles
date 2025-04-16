bf8542701e793c4a0f2759c1d35132c480a4bba5
Brian McGillion <bmg.avoin@gmail.com>
2025-04-16 14:05:23 +0400

    version: Add system.configurationRevision
    This adds system.configurationRevision which can be accessed by running
    `nixos-version --json`
    
    Signed-off-by: Brian McGillion <bmg.avoin@gmail.com>

--- hosts/common.nix
+++ hosts/common.nix
@@ -144,6 +144,9 @@ in
       distributedBuilds = true;
     };
 
+    # only available when dirty
+    system.configurationRevision = if (self ? rev) then self.rev else self.dirtyShortRev;
+
     # Sometimes it fails if a store path is still in use.
     # This should fix intermediate issues.
     systemd.services.nix-gc.serviceConfig = {
